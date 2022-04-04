eb10915186c63e2e328e3715a7779cfb96c8c335
Zach Cullen <zacullen@gmail.com>
2022-04-04 12:18:54 -0500

    add hello moon view

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return HttpResponse("Hello, World!")
+
+def moon(request):
+  return HttpResponse("Hello, Moon!")
