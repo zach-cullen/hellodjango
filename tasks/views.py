8ae48d18f190d4dccf110e5144723f9919efc910
Zach Cullen <zacullen@gmail.com>
2022-04-04 15:05:08 -0500

    render list of tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
 def index(request):
-  return render(request, "tasks/index.html")
+  return render(request, "tasks/index.html", {
+    "tasks": tasks
+  })
+
