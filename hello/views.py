9d9ad1215e8143161896b9f95e77e4256b626ac8
Zach Cullen <zacullen@gmail.com>
2022-04-04 12:42:57 -0500

    render template for greeting

--- hello/views.py
+++ hello/views.py
@@ -6,4 +6,6 @@ def index(request):
   return render(request, "hello/index.html")
 
 def greet(request, name):
-  return HttpResponse(f"Hello, {name.capitalize()}!")
+  return render(request, "hello/greet.html", {
+    "name": name.capitalize()
+  })
