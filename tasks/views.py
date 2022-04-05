af31b954b6b807c4d95abfb6fc243eab70ead4a9
Zach Cullen <zacullen@gmail.com>
2022-04-05 12:40:22 -0500

    create form post method

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label="New Task")
+  priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -9,5 +14,16 @@ def index(request):
   })
 
 def add(request):
-  return render(request, "tasks/add.html")
+  if request.method == "POST":
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data["task"]
+      tasks.append(task)
+    else:
+      return render(request, "tasks/add.html", {
+        "form": form
+      })
+  return render(request, "tasks/add.html", {
+    "form": NewTaskForm()
+  })
 
